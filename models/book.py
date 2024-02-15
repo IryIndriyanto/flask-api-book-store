bbc7874c01b9f39f0e98670010663310cf9d25fb
fauzanalghifary <m.fauzanghifary@gmail.com>
2024-02-15 07:51:33 +0700

    add DAO

--- models/book.py
+++ models/book.py
@@ -8,3 +8,24 @@ class BookModel(db.Model):
     title = db.Column(db.String(80), nullable=False, unique=True)
     author = db.Column(db.String(80), nullable=False)
     price = db.Column(db.Float(precision=2), nullable=False)
+
+    @classmethod
+    def get_books(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_book(cls, book_id):
+        return cls.query.get_or_404(book_id)
+
+    def add_book(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_book(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update_book(self, book_data):
+        for key, value in book_data.items():
+            setattr(self, key, value)
+        db.session.commit()
--- resources/book.py
+++ resources/book.py
@@ -2,7 +2,6 @@ from flask_smorest import abort, Blueprint
 from flask.views import MethodView
 from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
-from db import db
 from models import BookModel
 from schemas import BookSchema
 
@@ -13,7 +12,7 @@ blp = Blueprint("books", "books", description="Operations on books", url_prefix=
 class Book(MethodView):
     @blp.response(200, BookSchema(many=True))
     def get(self):
-        return BookModel.query.all()
+        return BookModel.get_books()
 
     @blp.arguments(BookSchema)
     @blp.response(200, BookSchema)
@@ -21,8 +20,7 @@ class Book(MethodView):
         # book = BookModel(title=book_data["title"], author=book_data["author"], price=book_data["price"])
         book = BookModel(**book_data)
         try:
-            db.session.add(book)
-            db.session.commit()
+            book.add_book()
         except IntegrityError:
             abort(400, message="A book with that title already exist.")
         except SQLAlchemyError:
@@ -35,23 +33,17 @@ class Book(MethodView):
 class Book(MethodView):
     @blp.response(200, BookSchema)
     def get(self, book_id):
-        book = BookModel.query.get_or_404(book_id)
+        book = BookModel.get_book(book_id)
         return book
 
     @blp.arguments(BookSchema)
     @blp.response(200, BookSchema)
     def put(self, book_data, book_id):
-        book = BookModel.query.get_or_404(book_id)
-        # book.title = book_data["title"]
-        # book.author = book_data["author"]
-        # book.price = book_data["price"]
-
-        BookModel.query.filter_by(id=book_id).update(book_data)
-        db.session.commit()
+        book = BookModel.get_book(book_id)
+        book.update_book(book_data)
         return book
 
     def delete(self, book_id):
-        book = BookModel.query.get_or_404(book_id)
-        db.session.delete(book)
-        db.session.commit()
+        book = BookModel.get_book(book_id)
+        book.delete_book()
         return {"message": "Book deleted"}
